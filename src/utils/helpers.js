3cf2039968d84baad1badded1d84bf739b0f8e9b
Deniz <dsimsek@octovan.com>
2020-06-24 22:48:14 +0300

    Added helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,2 +1,5 @@
 export const numberWithSeperator = (number, seperator) =>
   number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, seperator);
+
+export const numberWithK = number =>
+  number % 1000 !== number ? `${number / 1000}K` : number;
